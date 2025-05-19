66819645f7ade453cb38d4402e250107f6f1910a
wowcircledeveloper <pooldiver21@mail.ru>
2025-05-19 04:02:40 +0300

    release FPS_Doom_2.0

--- Assets/Scripts/AuthAndMenu/ReportGeneratorController.cs
+++ Assets/Scripts/AuthAndMenu/ReportGeneratorController.cs
@@ -3,27 +3,31 @@ using System.IO;
 using ReportGenerator;
 using UnityEngine.UI;
 using QuestPDF.Infrastructure;
+
 public class ReportGeneratorController : MonoBehaviour
 {
+    public Text FeedbackText; 
+
     public void GenerateReport()
     {
         QuestPDF.Settings.License = LicenseType.Community;
-        // Путь к users.json
         string jsonPath = Path.Combine(Application.persistentDataPath, "users.json");
-
-        // Путь для сохранения отчета
         string outputPath = Path.Combine(Directory.GetCurrentDirectory(), "report.pdf");
         string currentAccountId = PlayerPrefs.GetString("CurrentAccountId");
 
         try
         {
-            // Вызов метода генерации с тремя параметрами
             Report.GenerateReport(jsonPath, outputPath, currentAccountId);
-            Debug.Log("Отчет создан: " + outputPath);
+            FeedbackText.text = "Отчет создан: " + outputPath;
         }
         catch (System.Exception ex)
         {
-            Debug.LogError("Ошибка: " + ex.Message);
+            string errorMessage = "Ошибка: " + ex.Message;
+            Debug.LogError(errorMessage);
+
+            // Вывод ошибки в UI
+            if (FeedbackText != null)
+                FeedbackText.text = errorMessage;
         }
     }
 }
